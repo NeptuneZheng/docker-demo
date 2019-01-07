a0ac0778710698bd9bf6208798bf3c8f9bdde6b7
Neptune Zheng <neptune.zhenge@oocl.com>
2019-01-07 20:21:49 +0800

    add sh file

---
+++ src/docker_image_build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "buildDocker now ...."
+echo "===================="
+#setup variables
+APP_NAME="docker-demo"
+APP_VERSION="1.0.$BUILD_NUMBER"
+IMAGE_NAME="zhengne/$APP_NAME:$BUILD_NUMBER"
+echo $IMAGE_NAME
+
+docker build -t $IMAGE_NAME .
+docker tag demo/docker-demo:1.0.0 $IMAGE_NAME
+docker push $IMAGE_NAME
+
+echo "===================="
