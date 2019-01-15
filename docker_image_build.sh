eb8420342291055886b4f3c40d6e556afe3a9a5f
Neptune Zheng <neptune.zhenge@oocl.com>
2019-01-15 20:57:25 +0800

    add sudo

--- docker_image_build.sh
+++ docker_image_build.sh
@@ -8,7 +8,8 @@ APP_VERSION="1.0.$BUILD_NUMBER"
 IMAGE_NAME="zhengne/$APP_NAME:$BUILD_NUMBER"
 echo $IMAGE_NAME
 
-docker build -t $IMAGE_NAME .
-docker push $IMAGE_NAME
+sudo docker -v
+sudo docker build -t $IMAGE_NAME .
+sudo docker push $IMAGE_NAME
 
 echo "===================="
