a9b6605d4201532b9a974a5e2e1a497fbefa2aac
Neptune Zheng <neptune.zhenge@oocl.com>
2019-01-19 18:13:50 +0800

    use personal registry

--- docker_image_build.sh
+++ docker_image_build.sh
@@ -5,7 +5,7 @@ echo "===================="
 #setup variables
 APP_NAME="docker-demo"
 APP_VERSION="1.0.$BUILD_NUMBER"
-IMAGE_NAME="zhengne/$APP_NAME:$BUILD_NUMBER"
+IMAGE_NAME="127.0.0.1:5000/$APP_NAME:$BUILD_NUMBER"
 echo $IMAGE_NAME
 
 docker -v
