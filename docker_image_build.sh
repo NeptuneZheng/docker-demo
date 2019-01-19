5bf1f4e83baea7c393d189dd14b1a81e80d4de56
Neptune Zheng <neptune.zhenge@oocl.com>
2019-01-19 17:40:04 +0800

    remove useless sudo

--- docker_image_build.sh
+++ docker_image_build.sh
@@ -8,8 +8,8 @@ APP_VERSION="1.0.$BUILD_NUMBER"
 IMAGE_NAME="zhengne/$APP_NAME:$BUILD_NUMBER"
 echo $IMAGE_NAME
 
-sudo docker -v
-sudo docker build -t $IMAGE_NAME .
-sudo docker push $IMAGE_NAME
+docker -v
+docker build -t $IMAGE_NAME .
+docker push $IMAGE_NAME
 
 echo "===================="
